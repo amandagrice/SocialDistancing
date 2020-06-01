09c248f8adc86fda8ff07506ca19d8c5e98adb98
Amanda Grice <algrice@bu.edu>
2020-05-31 21:14:44 -0400

    make food float and spin

---
+++ Assets/scripts/Floating.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+
+public class Floating : MonoBehaviour {
+
+    public float degreesPerSecond = 15.0f;
+    public float amplitude = 0.1f;
+    public float frequency = 1f;
+ 
+    Vector3 posOffset = new Vector3 ();
+    Vector3 tempPos = new Vector3 ();
+ 
+    void Start () {
+        posOffset = transform.position;
+        if (Random.value > 0.5f) {
+    		amplitude = amplitude * -1;
+    	}
+    }
+     
+    void Update () {
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * frequency) * amplitude;
+        transform.position = tempPos;
+    }
+}
