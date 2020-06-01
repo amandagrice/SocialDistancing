d1c5c684809b5d30b1d7bd352086510bd91eadad
Amanda Grice <algrice@bu.edu>
2020-05-31 23:14:00 -0400

    add lose condition

---
+++ Assets/scripts/GetSick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetSick : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Zombie")) {
+			Application.LoadLevel(3);
+		}
+    }
+
+}
