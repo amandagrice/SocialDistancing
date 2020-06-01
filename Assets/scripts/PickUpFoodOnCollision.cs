ecb3a7477e668cb99ca08c03c19da4bf3fb94ba1
Amanda Grice <algrice@bu.edu>
2020-05-31 20:54:19 -0400

    add pick up animations

--- Assets/scripts/PickUpFoodOnCollision.cs
+++ Assets/scripts/PickUpFoodOnCollision.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class PickUpFoodOnCollision : MonoBehaviour {
 
+    private Animator[] Animators;
+	public static GameObject CollidedItem;
+
+	private void Start() {
+        Animators = FindObjectsOfType<Animator>();
+    }
+
 	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.CompareTag("Food")) {
-        	Destroy(other.gameObject);
-		}
+		CollidedItem = other.gameObject;
+    }
+
+     void OnTriggerExit() {
+     	CollidedItem = null;
+     }
+
+     void Update() {
+     	if (Input.GetKeyDown(KeyCode.E) && CollidedItem && CollidedItem.CompareTag("Food")) {
+     		Animators[0].SetTrigger("Pickup");
+            Destroy(CollidedItem);
+        }
      }
 
 }
